eaedea4f3cda2641456905a96a9cc1febea1dda6
thejaswi13 <115213739+thejaswi13@users.noreply.github.com>
2024-04-17 17:15:51 +0530

    updated server

---
+++ server/utils/connectDB.js
@@ -0,0 +1,14 @@
+//require('dotenv').config();
+import mongoose from "mongoose";
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log("Database Connected");
+  } catch (error) {
+    console.log("DB Error: " + error);
+  }
+};
+
+export default dbConnection;
