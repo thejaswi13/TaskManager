2ae65846d3161f39e0a116a2af7e49ab15b45a87
thejaswi13 <115213739+thejaswi13@users.noreply.github.com>
2024-04-18 16:39:08 +0530

    Update index.js

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,16 +1,22 @@
 import jwt from "jsonwebtoken";
 
 const createJWT = (res, userId) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
-  });
+  try {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
 
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
-    sameSite: "strict", // Prevent CSRF attacks
-    maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
-  });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+    });
+    
+    console.log("Token created and cookie set successfully.");
+  } catch (error) {
+    console.error("Error creating JWT token:", error);
+  }
 };
 
 export default createJWT;
