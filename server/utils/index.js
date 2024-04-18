c48732f1a2b466826eea38969039e031aa28b9f7
thejaswi13 <115213739+thejaswi13@users.noreply.github.com>
2024-04-18 16:40:59 +0530

    Update index.js

--- server/utils/index.js
+++ server/utils/index.js
@@ -8,7 +8,7 @@ const createJWT = (res, userId) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.NODE_ENV !== "development",
       sameSite: "strict",
       maxAge: 1 * 24 * 60 * 60 * 1000,
     });
