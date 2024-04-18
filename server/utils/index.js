c086c9b2b44b47b6c6dfa0c1ddb4c6915997c63e
thejaswi13 <115213739+thejaswi13@users.noreply.github.com>
2024-04-18 16:37:09 +0530

    Update index.js

--- server/utils/index.js
+++ server/utils/index.js
@@ -3,13 +3,12 @@ import jwt from "jsonwebtoken";
 const createJWT = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "1d",
-    sameSite: "none",
   });
 
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
-    sameSite: "none", // Prevent CSRF attacks
+    sameSite: "strict", // Prevent CSRF attacks
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
   });
 };
